410dcd0705e609bd13591d331183b7968087e277
AndrewVu <Andrewvu2001@hotmail.com>
2025-03-12 17:37:53 -0400

    Added so you can search any base card.

--- Code.java
+++ Code.java
@@ -5,22 +5,27 @@ import java.net.URL;
 public class Code {
     public static void main(String[] args){
       try{
-        URL url = new URL ("https://api.pokemontcg.io/v2/cards/base1-3");
+        Scanner Userinp = new Scanner(System.in);
+
+        System.out.println("Enter Base card ID (base1-1 and etc)");
+        String BcID = Userinp.nextLine();
+        Userinp.close();
+        URL url = new URL ("https://api.pokemontcg.io/v2/cards/" + BcID);
 
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
-
+        
         int response = connection.getResponseCode();
 
         if (response != 200){
           throw new RuntimeException("http Response Code" + response);
         } else{
-          Scanner scanner= new Scanner(url.openStream());
+          Scanner urlInfo = new Scanner(url.openStream());
           StringBuilder UrlResponse = new StringBuilder();
-          while (scanner.hasNextLine()){
-            UrlResponse.append(scanner.nextLine());
+          while (urlInfo.hasNextLine()){
+            UrlResponse.append(urlInfo.nextLine());
           }
-
+          urlInfo.close();
           System.out.println(UrlResponse);
         }
 
